4c793aef2df1285f75fd5aea30e6ff2e1d480927
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-10-30 12:29:54 +0000

    mas codigo

--- src/main/java/es/ull/esit/app/App.java
+++ src/main/java/es/ull/esit/app/App.java
@@ -6,5 +6,17 @@ package es.ull.esit.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // bucle que imprime los numeros del 1 al 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // codigo que divide por 0
+        try {
+            int resultado = 10 / 0;
+            System.out.println("El resultado es: " + resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: No se puede dividir por cero.");
+        }
     }
+
 }
